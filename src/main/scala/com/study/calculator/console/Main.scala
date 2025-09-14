ed0a3df2c78b35e26272d8c1c8041163c554305a
Bohdan Martines <bohdan.explorer@gmail.com>
2025-09-14 20:09:40 +0200

    Simple repl

--- src/main/scala/com/study/calculator/console/Main.scala
+++ src/main/scala/com/study/calculator/console/Main.scala
@@ -1,10 +1,22 @@
 package com.study.calculator.console
 
+import scala.io.StdIn.readLine
+
 object Main {
   enum Operation:
     case Add, Substruct, Multiply, Divide, Power
 
+  def repl(): Unit = {
+    val input = readLine("calc> ")
+    println(s"You entered [$input]")
+    if input == null || input.trim == ":q" then
+      print("bye!")
+    else
+      repl()
+  }
+
   @main def calculator() =
     println("Console Calculator. Print :q to quit")
     println("Examples:  2 + 3 | 3 * 4 | 3 ^ 2")
+    repl()
 }
