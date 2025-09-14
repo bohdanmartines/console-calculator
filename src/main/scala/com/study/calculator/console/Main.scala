844a669d44c540731055972d8c6fbe1bd0e5439b
Bohdan Martines <bohdan.explorer@gmail.com>
2025-09-14 19:49:28 +0200

    Create main method

---
+++ src/main/scala/com/study/calculator/console/Main.scala
@@ -0,0 +1,10 @@
+package com.study.calculator.console
+
+object Main {
+  enum Operation:
+    case Add, Subscruct, Multiply, Divide, Power
+
+  @main def calculator() =
+    println("Console Calculator. Print :q to quit")
+    println("Examples:  2 + 3 | 3 * 4 | 3 ^ 2")
+}
