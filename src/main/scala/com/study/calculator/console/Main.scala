5d54cc2e52605c98f992221d14d8de19ac7867c2
Bohdan Martines <bohdan.explorer@gmail.com>
2025-09-20 13:14:33 +0200

    Handle empty input in REPL gracefully by triggering `repl` recursively

--- src/main/scala/com/study/calculator/console/Main.scala
+++ src/main/scala/com/study/calculator/console/Main.scala
@@ -4,6 +4,7 @@ import scala.annotation.tailrec
 import scala.io.StdIn.readLine
 
 val LinePrefix = "calc>"
+val Pattern = raw"(\d+)\s*([+\-*/])\s*(\d+)".r
 
 @main def app(): Unit =
   println("Welcome to Console Calculator!")
@@ -11,11 +12,17 @@ val LinePrefix = "calc>"
 
 @tailrec
 def repl(): Unit =
-  println("Type the operation. Examples: 1 + 2 | 4 - 1 | 2 * 3 | 6 / 2")
+  println("Type an operation. Examples: 1 + 2 | 4 - 1 | 2 * 3 | 6 / 2")
   val input = readLine(LinePrefix)
   println(s"You entered [$input]")
 
   if input == null || input.equals(":q") then
     println("Bye!")
+  else if input.trim.isEmpty then
+    repl()
   else
+    // TODO Add handling user input
+    input match
+      case Pattern(a, operation, b) => println("Valid input")
+      case other => println("Invalid input")
     repl()
